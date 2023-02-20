1791ff0f7e105e1ef840cee365da0f38c2f7248e
molodoi <mr_stat12@hotmail.com>
2023-02-20 12:23:40 +0100

    Add FOSCKEditorBundle on PostType content

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -11,6 +11,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Post;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -43,7 +44,7 @@ class PostType extends AbstractType
                     new Assert\NotNull(),
                 ],
             ])
-            ->add('content')
+            ->add('content', CKEditorType::class)
             ->add('thumbFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => true,
